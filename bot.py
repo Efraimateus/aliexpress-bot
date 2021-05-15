5c1685a5f3e4b0b377aa07dc571b5b93faf4c1bb
matheus <mths@edu.unifor.br>
2021-05-15 02:00:42 -0300

    email generator added

--- bot.py
+++ bot.py
@@ -4,9 +4,19 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
-
 from selenium.webdriver.common.by import By
+import itertools
+import random
 
+# gera emails aleatorios 
+letters = ["a", "b","c", "d","e", "f","g", "h","i", "j","k", "l","m", "n", "o", "p","q", "r","s", "t","u", "v","w", "x","y", "z",]
+all_combos = list(itertools.combinations(letters,7)) 
+all_combos = [''.join(combo) for combo in all_combos] 
+email = random.sample(all_combos,1)[0]+'@gmail.com' 
+
+password = "Senhaestranha1#"
+
+# emulador de celular pq a pagina so funciona em modo mobile 
 mobile_emulation = {
 
    "deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },
@@ -29,13 +39,6 @@ driver.find_element_by_xpath('//*[@id="msite-login"]/div/div[2]/div[1]/div/ul/li
 
 time.sleep(2)
 driver.find_element_by_xpath('//*[@id="msite-login"]/div/div[2]/div[1]/div/ul/li[1]/div').click()
-
-##########################################################################
-#                                                                        # 
-email = "txxxcuxamaeaquelasafada23ixxa@asdssdi.sdsd"                      #
-password = "apdasdsadas"                                                 #
-#                                                                        #
-# ########################################################################    
                                                                     
 time.sleep(3)
 driver.find_element_by_xpath('//*[@id="msite-login"]/div/div[2]/div[2]/div[1]/div/div/div/div[4]/div[1]/input').send_keys(email)
